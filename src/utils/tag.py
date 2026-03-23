f83ea109d9058cafe2b9938b4beb965337d6d5f7
Mountainsw <mountianeerz@gmail.com>
2026-03-22 23:51:19 -0700

    Refactor Strtag method in tags class
    Refactor Strtag method to improve readability and functionality.

--- src/utils/tag.py
+++ src/utils/tag.py
@@ -6,22 +6,18 @@ import sql
 db = Path("src/db/test.db")
 
 class tags:
-    
     @staticmethod
     
     def Strtag(str):
         dbm = sql.SqlManagement(db)
-        
         f, l = str[0], str[-1]
         s = f + l
         print(f"tag created for string {str}: {f+l}")
         
         dbm.IDtablecommit("tags", str, s)
-        
         return s
 
         
         
         
         
-tags.Strtag("hello")
